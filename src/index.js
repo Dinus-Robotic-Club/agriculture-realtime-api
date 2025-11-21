ff95d77751c2559d8f6b868ee385cebf3176845d
zappto <saptogusty@gmail.com>
2025-11-21 20:53:23 +0700

    iya

--- src/index.js
+++ src/index.js
@@ -21,7 +21,11 @@ const io = new Server(server, {
 })
 
 // Middleware
-app.use(cors())
+app.use(
+    cors({
+        origin: '*',
+    }),
+)
 app.use(express.json())
 
 app.use('/api/sensor', sensorRoutes(io))
