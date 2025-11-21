249422b239f9c0db00426a60bd0d13ed78cb84c9
zappto <saptogusty@gmail.com>
2025-11-21 20:49:32 +0700

    iyah

--- src/routes/sensor.routes.js
+++ src/routes/sensor.routes.js
@@ -1,11 +1,21 @@
 import express from 'express'
-import { createSensorDataController, createManySensorDataController } from '../controller/sensor.controller.js'
+import {
+    createSensorDataController,
+    createManySensorDataController,
+    getSesnsorDatabyTypeController,
+    getSensorDatabyIDController,
+    getSensorDataController,
+} from '../controller/sensor.controller.js'
 
 export default (wss) => {
     const router = express.Router()
 
     router.post('', createSensorDataController(wss))
     router.post('/many', createManySensorDataController(wss))
+    router.get('', getSensorDataController)
+    router.get('/:id', getSensorDatabyIDController)
+    router.get('/:type', getSesnsorDatabyTypeController)
 
     return router
 }
+sensor
